c47a6cc3be19f4381675a0ea46c1b6cf29580307
rafi.dhega@gmail.com <rafi.dhega@gmail.com>
2020-12-21 11:43:30 +0700

    init project

---
+++ app/src/main/java/rafi/sekolahku/FormActivity.kt
@@ -0,0 +1,11 @@
+package rafi.sekolahku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
---
+++ app/src/main/java/rafi/sekolahku/TestActivity.kt
@@ -0,0 +1,11 @@
+package rafi.sekolahku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.spinner_sample)
+    }
+}
